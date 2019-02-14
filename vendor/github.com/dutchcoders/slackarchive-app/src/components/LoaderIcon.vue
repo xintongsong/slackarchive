14dfa45fc29acad2f1fcffa4be020decc4dfe4a8
Ash Berlin-Taylor <ash_github@firemirror.com>
2019-02-14 22:07:34 +0000

    Remove tests and non-go files from vendor/
    We don't need them, they just create extra noise

--- vendor/github.com/dutchcoders/slackarchive-app/src/components/LoaderIcon.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div v-show="loading" class="loader" :class="'loader-' + (size ? size : 'xs')">{{size}}</div>
-</template>
-
-<script>
-  export default {
-    props: ['loading', 'size']
-  }
-</script>
